5e1d603a3b1186797c1bc2bdf435db783263132b
Rianna Suen <vividfax@gmail.com>
2019-04-14 19:28:33 +0100

    Split js files

---
+++ js/makeNotes.js
@@ -0,0 +1,27 @@
+function makenote0(time, playbackRate) {
+    bassoon0.play(time, 1, 1, 0, noteDuration * 1);
+}
+
+function makeNote1(time, playbackRate) {
+    bassoon1.play(time, 1.5, 1, 0, noteDuration * 1.5);
+}
+
+function makeNote2(time, playbackRate) {
+    bassoon2.play(time, 2, 1, 0, noteDuration * 2);
+}
+
+function makeNote3(time, playbackRate) {
+    bassoon3.play(time, 2.5, 1, 0, noteDuration * 2.5);
+}
+
+function makeNote4(time, playbackRate) {
+    bassoon4.play(time, 3, 1, 0, noteDuration * 3);
+}
+
+function makeNote5(time, playbackRate) {
+    bassoon5.play(time, 3.5, 1, 0, noteDuration * 3.5);
+}
+
+function makeNote6(time, playbackRate) {
+    bassoon6.play(time, 4, 1, 0, noteDuration * 4);
+}
